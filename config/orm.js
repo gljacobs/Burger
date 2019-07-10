6bcfeff3491d63707c86b594b4c354fd7822db17
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-07-09 22:01:33 -0700

    finish config set up with orm

---
+++ config/orm.js
@@ -0,0 +1,82 @@
+var connection = require("../config/connection.js");
+
+//helper func
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+//helper func
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
+// Object for all our SQL statement functions.
+var orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, callback) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, res) {
+      if (err) {
+        throw err;
+      }
+
+      callback(res);
+    });
+  },
+  // vals ex: {burger_name: cheesyburger, devoured: true}
+  updateOne: function(table, vals, condition, callback) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(vals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      callback(res);
+    });
+  }
+};
+
+module.exports = orm;
